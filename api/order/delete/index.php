68601f9c23ff96620b5c05785cf0ad13dd9e94ec
Jorge <desarrollo2@pcnub.com>
2020-06-28 22:10:44 -0500

    first commit

---
+++ api/order/delete/index.php
@@ -0,0 +1,22 @@
+<?php
+
+// Borrar o cancelar una orden creada
+
+header('Content-type: application/json');
+
+include_once '../../global/functions.php';
+global $access_token,$collector_id;
+
+$external_id = $_REQUEST["external_id"];
+ 
+ // REVISA AQUÍ:
+ // Qué método y endpoint de la API de Mercado Pago deberías poner aquí para poder eliminar una orden
+ // en base al external_id, collector_id recibidos
+ // Pista... revísalo bien...
+ // Sustituye el método por su correspondiente: get, put, post, delete
+
+ $url = "https://api.mercadopago.com/mpmobile/instore/qr/$collector_id/$external_id";
+
+curl_call("delete","$url?access_token=$access_token", "");
+
+?>
